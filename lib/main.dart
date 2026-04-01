ad0d8f8199a938f4cbadfbb17802254e4258d92c
Gogos <fnavarretecortes63@gmail.com>
2026-04-01 18:32:13 -0300

    Layout principal y tablero

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,68 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Buscaminas',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Text('Hello, World!'),
+      home: const MinesweeperScreen(),
     );
   }
+}
+
+class MinesweeperScreen extends StatelessWidget {
+  const MinesweeperScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Buscaminas')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // Status
+            Container(
+              height: 60,
+              color: Colors.grey[300],
+              child: const Center(
+                child: Text(
+                  'STATUS: 349 segundos | Minas: 10 | Cuadros: 56',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ),
+            ),
+            const Divider(height: 1),
+
+            // Área de Juego
+            Expanded(
+              child: _gameBoard(), // 👈 AQUÍ se usa
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _gameBoard() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AspectRatio(
+          aspectRatio: 1.0,
+          child: GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 8, // 8 columnas
+              crossAxisSpacing: 2.0,
+              mainAxisSpacing: 2.0,
+            ),
+            itemCount: 64, // 8x8 = 64 celdas
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                color: Colors.grey[400],
+                border: Border.all(color: Colors.grey[600]!, width: 1.5),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+}
 }
